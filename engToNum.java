8870f8146071a03d8c248cfdbddace18a136d05c
Frog <84081437+huiji-park@users.noreply.github.com>
2023-09-07 14:09:50 +0900

    Create engToNum.java
    숫자 문자열과 영단어

---
+++ engToNum.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String s) {
+        String word [] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        
+        for(int i = 0; i < 10; i++)
+            s = s.replaceAll(word[i], Integer.toString(i));
+        
+        return Integer.parseInt(s);
+    }
+}
